23937bd1d5ad8efe9468d46614283998a0340c8e
unknown <mpayne@PAYNEW7x64.danteconsulting.com>
2014-03-29 20:13:52 -0400

    first cut of local storage capability

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,18 +1,24 @@
 'use strict';
 
 angular.module('ntApp').controller('NotesCtrl', function ($scope) {
-	  $scope.newNote = {};
-    $scope.notes = [{
-    	txt : "don't forget to walk the dog"	
-    },
-    {   
-    	txt : "pick up some pickles on the way home"
-    },
-    {
-    	txt : "why do I always trip over my shoe laces?"
-    }];
+$scope.newNote = "";
+    $scope.notes = ["don't forget to walk the dog", "pick up some pickles on the way home", "why do I always trip over my shoe laces?"];
+	
+	for (var i = 0; i < localStorage.length; i++) {
+		var key = localStorage.key(i);
+	
+		$scope.notes.push(localStorage.getItem(key));
+	}
+
     $scope.saveNote = function(){
-    	$scope.notes.push($scope.newNote)
-    	$scope.newNote = {};
+		 localStorage.setItem($.now(), $scope.newNote);
+		 $scope.notes.push($scope.newNote);
+		 $scope.newNote = "";
+    }
+	
+	//for convenience atm
+	$scope.removeAll = function(){
+		localStorage.clear();
+		document.location.reload(true);
     }
   });
