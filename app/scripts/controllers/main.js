4f2939c1f971eac26efbbe3a4bd9631e0dae2bb4
cantide5ga <michael.payne@danteinc.com>
2014-03-31 17:00:59 -0400

    fist cut remote-storage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,18 +1,24 @@
 'use strict';
 
-angular.module('ntApp').controller('NotesCtrl', function ($scope) {
-	  $scope.newNote = {};
-    $scope.notes = [{
-    	txt : "don't forget to walk the dog"	
-    },
-    {   
-    	txt : "pick up some pickles on the way home"
-    },
-    {
-    	txt : "why do I always trip over my shoe laces?"
-    }];
+angular.module('ntApp').controller('NotesCtrl', function ($scope, $http) {
+	$scope.newNote = "";
+    $scope.notes = ["don't forget to walk the dog", "pick up some pickles on the way home", "why do I always trip over my shoe laces?"];
+	
+	var serviceUrl = "http://localhost:8080/notetaker/api/note/";
+	
+	$http.get(serviceUrl + 'query').
+        success(function(data) {
+			for (var i = 0; i < data.length; i++) {
+						$scope.notes.push(data[i].message);
+			}
+        }).
+		error(function(error, status) {
+        alert(status + error.message);
+    });
+
     $scope.saveNote = function(){
-    	$scope.notes.push($scope.newNote)
-    	$scope.newNote = {};
+		 $http.post(serviceUrl, $scope.newNote);
+		 $scope.notes.push($scope.newNote);
+		 $scope.newNote = "";
     }
   });
